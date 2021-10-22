e0f35e4d7a421f1e2e95d69dea6e568588366b48
이정준 <junnnyyy@kakao.com>
2021-10-22 10:28:14 +0900

    #S05P31B203-110 feat: Customer Entity 관련 생성/be

---
+++ backend/runner/src/main/java/ssafy/runner/domain/entity/Customer.java
@@ -0,0 +1,13 @@
+package ssafy.runner.domain.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Customer {
+}
