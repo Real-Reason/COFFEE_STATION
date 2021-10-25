536f54a958885f5e0650d08c82c1796367befa23
wns312 <wns312@gmail.com>
2021-10-25 16:20:58 +0900

    #S05P31B203-179 feat: Custome Service & Repository 생성

---
+++ backend/runner/src/main/java/ssafy/runner/service/CustomerService.java
@@ -0,0 +1,16 @@
+package ssafy.runner.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ssafy.runner.domain.repository.CustomerRepository;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    
+}
