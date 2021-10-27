14b95300ccef42faabd296f0440e81a66b61701e
wns312 <wns312@gmail.com>
2021-10-27 14:16:25 +0900

    #S05P31B203-161 feat: 메뉴 리스트 조회

--- backend/runner/src/main/java/ssafy/runner/controller/partner/MenuController.java
+++ backend/runner/src/main/java/ssafy/runner/controller/partner/MenuController.java
@@ -4,13 +4,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ssafy.runner.domain.dto.ShopReqDto;
 import ssafy.runner.domain.dto.partner.MenuCreateRequestDto;
 import ssafy.runner.domain.dto.partner.MenuCreateResponseDto;
+import ssafy.runner.domain.dto.partner.MenuListResponseDto;
 import ssafy.runner.domain.enums.UserType;
 import ssafy.runner.service.MenuService;
 import ssafy.runner.util.CustomPrincipal;
@@ -26,8 +24,24 @@ public class MenuController {
     @PostMapping("")
     @ApiOperation(value = "메뉴 생성")
     public MenuCreateResponseDto createMenu(Authentication authentication, @RequestBody MenuCreateRequestDto requestDto) {
+        System.out.println(requestDto);
         CustomPrincipal principal = (CustomPrincipal) authentication.getPrincipal();
         if (principal.getRole().equals(UserType.CUSTOMER.toString())) throw new RuntimeException("점주가 아니면 메뉴를 생성할 수 없습니다.");
-        return menuService.createMenu(principal.getEmail(), requestDto.getCategoryId(), requestDto.getPrice(), requestDto.getImgUrl(), requestDto.isSignature());
+        return menuService.createMenu(principal.getEmail(), requestDto.getCategoryId(), requestDto.getPrice(), requestDto.getName(), requestDto.getImgUrl(), requestDto.isSignature());
+    }
+
+    @GetMapping("")
+    @ApiOperation(value = "메뉴 리스트 조회")
+    public MenuListResponseDto findMenuList(Authentication authentication) {
+        // 가게용 조회이므로
+        CustomPrincipal principal = (CustomPrincipal) authentication.getPrincipal();
+        if (principal.getRole().equals(UserType.CUSTOMER.toString())) throw new RuntimeException("점주가 아니면 메뉴를 조회할 수 없습니다.");
+
+        return menuService.findShopMenus(principal.getEmail());
+    }
+
+    @PostMapping("/{menuId}")
+    @ApiOperation(value = "메뉴 단일 조회")
+    public void findOneMenu(Authentication authentication, @PathVariable("menuId") Long menuId) {
     }
 }
--- backend/runner/src/main/java/ssafy/runner/domain/repository/MenuRepository.java
+++ backend/runner/src/main/java/ssafy/runner/domain/repository/MenuRepository.java
@@ -1,7 +1,16 @@
 package ssafy.runner.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ssafy.runner.domain.entity.Menu;
+import ssafy.runner.domain.entity.Shop;
+
+import java.util.List;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+    // 카테고리도 같이 조회해야 함
+
+    @Query("select m from Menu m join fetch m.category c where m.shop.id = :shopId")
+    List<Menu> findAllByShopWithCategory(@Param("shopId") Long shopId);
 }
