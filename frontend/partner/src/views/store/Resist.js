7cdadb1b719f2f8ee66286fd4753370f0d6a7b3c
jnheo1216 <hjeionyng94@gmail.com>
2021-10-28 10:47:19 +0900

    #S05P31B203-210 feat: partner 화면들 생성

---
+++ frontend/partner/src/views/store/Resist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Resist = () => {
+  return (
+      <View>
+        <Text>Resist</Text>
+      </View>
+  );
+}
+
+export default Resist;
