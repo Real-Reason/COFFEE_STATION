94efe4edeb4f73dd7e9595374a5c1807407f6d72
jnheo1216 <hjeionyng94@gmail.com>
2021-11-06 00:48:51 +0900

    #S05P31B203-303 feat: react native map을위한 webview

--- frontend/customer/src/views/cafe/MainScreen.js
+++ frontend/customer/src/views/cafe/MainScreen.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, Button } from 'react-native';
 import axios from 'axios';
+import Geolocation from '@react-native-community/geolocation';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Maps from '../map/Maps';
 
-const MainScreen = () => {
+
+const Stack = createNativeStackNavigator();
+
+const MainCafeList = ({ navigation }) => {
   const [cafeList, setCafeList] = useState('')
 
   const getInfo = () => {
@@ -14,6 +20,23 @@ const MainScreen = () => {
     // catch (e) {
     //   console.logg(e);
     // }
+  }
+
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLogitude] = useState(null);
+
+  const geoLocation = () => {
+      Geolocation.getCurrentPosition(
+          position => {
+              const latitude = JSON.stringify(position.coords.latitude);
+              const longitude = JSON.stringify(position.coords.longitude);
+
+              setLatitude(latitude);
+              setLogitude(longitude);
+          },
+          error => { console.log(error.code, error.message); },
+          {enableHighAccuracy:true, timeout: 15000, maximumAge: 10000 },
+      )
   }
 
   useEffect(() => {
@@ -31,8 +54,25 @@ const MainScreen = () => {
           justifyContent: 'center'
         }}
       >
+        <Button title="Button" onPress={() => navigation.navigate('Maps')}></Button>
         <Text>MainScreen</Text>
+        <Pressable onPress={() => geoLocation()}>
+            <Text> Get GeoLocation </Text>
+        </Pressable>
+        <Text> latitude?: {latitude} </Text>
+        <Text> longitude?: {longitude} </Text>
       </View>
+  );
+}
+
+
+const MainScreen = () => {
+
+  return (
+      <Stack.Navigator>
+        <Stack.Screen name="MainCafeList" component={MainCafeList} />
+        <Stack.Screen name="Maps" component={Maps} />
+      </Stack.Navigator>
   );
 }
 
