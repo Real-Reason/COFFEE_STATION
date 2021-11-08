226a815acb1c639f7d8e97692a3fe2f8fdd571e0
이정준 <junnnyyy@kakao.com>
2021-11-08 21:14:20 +0900

    #S05P31B203-297 feature: ddl-update/be

--- backend/runner/src/main/java/ssafy/runner/domain/repository/OrderMenuRepository.java
+++ backend/runner/src/main/java/ssafy/runner/domain/repository/OrderMenuRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ssafy.runner.domain.entity.Menu;
 import ssafy.runner.domain.entity.OrderMenu;
+import ssafy.runner.domain.entity.Orders;
 import ssafy.runner.domain.enums.OrderStatus;
 
 import java.util.List;
@@ -15,7 +16,6 @@ public interface OrderMenuRepository extends JpaRepository<OrderMenu, Long> {
     @Query("select sum(om.price * om.quantity) from OrderMenu om " +
             "where om.menu = :menu and om.order.status = :status")
     Integer findRevenueByMenu(@Param("menu") Menu menu, @Param("status") OrderStatus status);
-
 
     @Query("select om from OrderMenu om"+
             " join fetch om.order"+
