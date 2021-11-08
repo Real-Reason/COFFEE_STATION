33e5d33f9f66e8a674c07056004fc0ec6be8f47f
이유진 <eujjin@naver.com>
2021-11-08 11:27:11 +0900

    #S05P31B203-363 feat: 여러장 사진 upload test 완료

--- backend/runner/src/main/java/ssafy/runner/RunnerApplication.java
+++ backend/runner/src/main/java/ssafy/runner/RunnerApplication.java
@@ -2,6 +2,7 @@ package ssafy.runner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class RunnerApplication {
