75ed349cf852df5f7abc239ec16c070a4b59d726
이정준 <junnnyyy@kakao.com>
2021-11-09 15:12:51 +0900

    #S05P31B203-404 feature: 주문시 엑스트라 가격추가,queryDSL/be

--- backend/runner/src/main/java/ssafy/runner/domain/repository/ShopRepositoryImpl.java
+++ backend/runner/src/main/java/ssafy/runner/domain/repository/ShopRepositoryImpl.java
@@ -3,6 +3,7 @@ package ssafy.runner.domain.repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import ssafy.runner.domain.entity.QShop;
 import ssafy.runner.domain.entity.Shop;
 
 import java.util.List;
@@ -15,11 +16,10 @@ public class ShopRepositoryImpl implements ShopRepositoryCustom {
     private final JPAQueryFactory jpaQueryFactory;
     @Override
     public List<Shop> searchShop(String searchWord) {
-        return null;
-//        return jpaQueryFactory
-//                .select(shop)
-//                .from(shop)
-//                .where(shop.name.contains(searchWord))
-//                .fetch();
+        return jpaQueryFactory
+                .select(QShop.shop)
+                .from(QShop.shop)
+                .where(QShop.shop.name.contains(searchWord))
+                .fetch();
     }
 }
