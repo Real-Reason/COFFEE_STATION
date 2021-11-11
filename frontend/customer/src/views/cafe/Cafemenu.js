3873314974706fdc0ef6523e4b174e24a196458e
jnheo1216 <hjeionyng94@gmail.com>
2021-11-11 20:24:46 +0900

    #S05P31B203-379 feat: customer 장바구니담기

--- frontend/customer/src/views/cafe/Cafemenu.js
+++ frontend/customer/src/views/cafe/Cafemenu.js
@@ -1,31 +1,74 @@
-import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image, Button } from 'react-native';
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Cafemenu = ({ route }) => {
 
+  const [count, setCount] = useState(1)
+
   useEffect(() => {
     console.log(' cafe menu mount');
-    console.log(route.params);
+    console.log(route.params.id);
+    console.log(route.params.menuInfo.menuId);
     setCafeMenu();
-    return () => console.log(' cafe menu Unmount');
+    return () => console.log(' cafe menu Unmount!');
   }, []);
 
   const setCafeMenu = async() => {
     console.log('get Cafe Menu');
     try {
-      const response = await axios.get(`http://10.0.2.2:8080/api/customer/shop/${route.params.id}/menu/${route.params.menuId}`);
+      const response = await axios.get(`http://3.38.99.110:8080/api/customer/shop/${route.params.id}/menu/${route.params.menuInfo.menuId}`);
       console.log(response.data);
     } catch (e) {
       console.log(e);
     }
-
   }
 
+  const addCart = async(item) => {
+    console.log('카트에 추가!');
+    console.log('넣어야할것 : 가게id, 상품, 개수');
+    let tmp = JSON.parse(await AsyncStorage.getItem('cartList'));
+    let cartlist = {items: []};
+    let isSame = true;
+    if (tmp) {
+      let cartlistall = tmp.items
+      cartlistall.forEach((val, index) => {
+        console.log(index);
+        if (val.cafeId == route.params.id && val.menuId == route.params.menuInfo.menuId) {
+          console.log('중복0');
+          val.count = val.count + count;
+          isSame = false;
+          return false;
+        }
+      });
+      if (isSame) {
+        cartlistall.push({cafeId: route.params.id, item, count, menuId: route.params.menuInfo.menuId });
+        isSame = true;
+      }
+      cartlist = {items: cartlistall};
+    } else {
+      cartlist = {items: [{cafeId: route.params.id, item, count, menuId: route.params.menuInfo.menuId }]}
+    }
+    await AsyncStorage.setItem('cartList', JSON.stringify(cartlist));
+    alert(`장바구니에 ${route.params.menuInfo.name} 추가`);
+  }
 
   return (
       <View>
-        <Text>Cafemenu</Text>
+        <Image 
+          source={{uri: route.params.menuInfo.imgUrl }} 
+          style={{width:100, height:100}} 
+        />
+
+        <Text>{ route.params.menuInfo.price }</Text>
+        <Text>{ route.params.menuInfo.name }</Text>
+        <Text>{ route.params.menuInfo.menuStatus }</Text>
+        <Button title="-1" onPress={() => setCount(count-1)}></Button>
+        <Text>{ count }</Text>
+        <Button title="+1" onPress={() => setCount(count+1)}></Button>
+        <Button title="장바구니에 추가하기" onPress={() => addCart(route.params.menuInfo)}></Button>
+        
       </View>
   );
 }
--- frontend/customer/src/views/user/Cart.js
+++ frontend/customer/src/views/user/Cart.js
@@ -1,11 +1,58 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {  Text, Button, ScrollView, RefreshControl, Pressable } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Cart = () => {
+
+  const [cartList, setCartList] = useState({'items': []});
+  const [refreshing, setRefreshing] = useState(false);
+  const [isCart, setIsCart] = useState(false);
+
+  const getCartList = async() => {
+    console.log('장바구니가져와');
+    try {
+      setCartList(JSON.parse(await AsyncStorage.getItem('cartList')));
+      console.log(cartList);
+      console.log(typeof(cartList));
+      setIsCart(true);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const clearCartList = async() => {
+    try{
+      await AsyncStorage.removeItem('cartList');
+    } catch (e) {
+      console.log(e);
+    }
+    setCartList({'items': []});
+    setIsCart(false);
+  }
+
   return (
-      <View>
+      <ScrollView
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={getCartList} 
+          />
+        }
+      >
         <Text>Cart</Text>
-      </View>
+        {
+          isCart
+          ? (<Text></Text>)
+          : (<Text>없습니다.</Text>)
+        }
+        {cartList.items.map((cartmenu, index) => (
+              <Pressable key={index} onPress={() => alert(index)}>
+                <Text> { cartmenu.item.name }, {cartmenu.count}개, { cartmenu.item.price * cartmenu.count }원 </Text>
+              </Pressable>
+        ))}
+        <Button title='주문하기' onPress={() => alert('주문페이지 예정')}></Button>
+        <Button title='장바구니 비우기' onPress={() => clearCartList()}></Button>
+      </ScrollView>
   );
 }
 
