7e3b4f982b55d20b9ee3985af4a114f43ed79393
한건 <ofliberty@naver.com>
2021-11-15 00:07:08 +0900

    #S05P31B203-412 feat: signOut 수정

--- frontend/partner/src/App.js
+++ frontend/partner/src/App.js
@@ -7,13 +7,14 @@ import styled from 'styled-components/native';
 import axios from 'axios';
 import Sign from './registration/sign/Sign';
 import SignInScreen from './registration/sign/components/SignInScreen';
-import Main from './registration/store/Main';
+import RegiMain from './registration/store/RegiMain';
+import ManageMain from './management/ManageMain';
 import messaging from '@react-native-firebase/messaging';
 import {Alert} from 'react-native';
 
 const AuthContext = createContext();
 const Stack = createNativeStackNavigator();
-const baseURL = 'http://10.0.2.2:8080/api/partner'
+const baseURL = 'http://10.0.2.2:8080/api/partner';
 
 const SplashScreen = () => {
   return (
@@ -23,26 +24,17 @@ const SplashScreen = () => {
   );
 };
 
-const HomeScreen = () => {
-  const {signOut} = useContext(AuthContext);
-
-  return (
-    <View>
-      <Text>Signed in!</Text>
-      <Button title="Sign out" onPress={signOut} />
-    </View>
-  );
-};
-
 export default function App({navigation}) {
   let firebaseToken;
   const [state, dispatch] = React.useReducer(
     (prevState, action) => {
+      console.log('=-=-=-=-=-=-=- action =-=-=-=-=-=-=-', action);
       switch (action.type) {
-        case 'RESTORE_TOKEN':
+        case 'RESTORE_STATE':
           return {
             ...prevState,
             userToken: action.token,
+            hasRegistered: action.regi,
             isLoading: false,
           };
         case 'SIGN_IN':
@@ -50,12 +42,14 @@ export default function App({navigation}) {
             ...prevState,
             isSignout: false,
             userToken: action.token,
+            hasRegistered: JSON.parse(action.regi),
           };
         case 'SIGN_OUT':
           return {
             ...prevState,
             isSignout: true,
             userToken: null,
+            hasRegistered: false,
           };
         case 'SIGN_UP':
           return {
@@ -69,30 +63,33 @@ export default function App({navigation}) {
       isLoading: true,
       isSignout: false,
       userToken: null,
+      hasRegistered: false,
     },
   );
   useEffect(() => {
     const unsubscribe = messaging().onMessage(async remoteMessage => {
       // const orderId = remoteMessage['data'].orderId;
       // console.log(orderId);
-      Alert.alert(                    // 말그대로 Alert를 띄운다
-        "주문을 수락하시겠습니까?",        // 첫번째 text: 타이틀 제목
+      Alert.alert(
+        // 말그대로 Alert를 띄운다
+        '주문을 수락하시겠습니까?', // 첫번째 text: 타이틀 제목
         JSON.stringify(remoteMessage['notification']),
         [
           {
-          text: "수락하기", // 버튼 제목
-          onPress: () => {
-            updateOrderStatus(remoteMessage['data'].orderId, 'PREPARING')
-            console.log(remoteMessage['data'].orderId)
-            Alert.alert("주문을 수락했습니다.");
+            text: '수락하기', // 버튼 제목
+            onPress: () => {
+              updateOrderStatus(remoteMessage['data'].orderId, 'PREPARING');
+              console.log(remoteMessage['data'].orderId);
+              Alert.alert('주문을 수락했습니다.');
             },
           },
-          { text: "거절하기",
+          {
+            text: '거절하기',
             onPress: () => {
-            updateOrderStatus(remoteMessage['data'].orderId, 'REJECT')
-            console.log(remoteMessage['data'].orderId)
-            Alert.alert("주문을 거절했습니다.");
-            }
+              updateOrderStatus(remoteMessage['data'].orderId, 'REJECT');
+              console.log(remoteMessage['data'].orderId);
+              Alert.alert('주문을 거절했습니다.');
+            },
           },
         ],
         {cancelable: false},
@@ -108,7 +105,7 @@ export default function App({navigation}) {
       .getToken()
       .then(firebaseToken => {
         console.log(firebaseToken);
-        return saveTokenToDatabase({ firebaseToken });
+        return saveTokenToDatabase({firebaseToken});
       });
     // Listen to whether the token changes
     return messaging().onTokenRefresh(firebaseToken => {
@@ -116,50 +113,55 @@ export default function App({navigation}) {
     });
   }, []);
   // const baseURL = 'http://10.0.2.2:8080/api/partner'
-  const saveTokenToDatabase = async (data) => {
+  const saveTokenToDatabase = async data => {
     let userToken = await AsyncStorage.getItem('userToken');
-    console.log('====usertoken========'+userToken);
-    if ( userToken !== null ){
-      await axios.patch(baseURL + '/firebase-token', 
-      data,
-      {headers: {
-        'Authorization': "Bearer " + userToken
-        },
-      })
-      .then(res => {
-        console.log('success', res.data);
-      })
-      .catch(error => {
-      console.log("fail", error);
-      })
+    console.log('====usertoken========' + userToken);
+    if (userToken !== null) {
+      await axios
+        .patch(baseURL + '/firebase-token', data, {
+          headers: {
+            Authorization: 'Bearer ' + userToken,
+          },
+        })
+        .then(res => {
+          console.log('success', res.data);
+        })
+        .catch(error => {
+          console.log('fail', error);
+        });
     }
-  }
+  };
 
   const updateOrderStatus = async (orderId, status) => {
     let userToken = await AsyncStorage.getItem('userToken');
-    console.log('====usertoken========'+userToken);
-    await axios.patch(baseURL + `/shop/orders/${orderId}/status`,
-      {'status': status},
-      {headers: {
-        'Authorization': "Bearer " + userToken
-      },
-    })
-    .then(res => {
-      console.log("status 변경 완료", res.data);
-    })
-    .catch(error => {
-      console.log("status 변경 실패", error);
-    })
-  }
+    console.log('====usertoken========' + userToken);
+    await axios
+      .patch(
+        baseURL + `/shop/orders/${orderId}/status`,
+        {status: status},
+        {
+          headers: {
+            Authorization: 'Bearer ' + userToken,
+          },
+        },
+      )
+      .then(res => {
+        console.log('status 변경 완료', res.data);
+      })
+      .catch(error => {
+        console.log('status 변경 실패', error);
+      });
+  };
 
   useEffect(() => {
     // Fetch the token from storage then navigate to our appropriate place
     const bootstrapAsync = async () => {
       let userToken;
-
+      let hasRegistered;
       try {
         userToken = await AsyncStorage.getItem('userToken');
-        console.log('UserToken: ', userToken);
+        hasRegistered = JSON.parse(await AsyncStorage.getItem('hasRegistered'));
+        // console.log('UserToken: ', userToken);
       } catch (e) {
         console.log(e);
         // Restoring token failed
@@ -169,7 +171,8 @@ export default function App({navigation}) {
 
       // This will switch to the App screen or Auth screen and this loading
       // screen will be unmounted and thrown away.
-      dispatch({type: 'RESTORE_TOKEN', token: userToken});
+      // 레지스터도 리스토어 시켜줘야 할 거 같은데
+      dispatch({type: 'RESTORE_STATE', token: userToken, regi: hasRegistered});
     };
 
     bootstrapAsync();
@@ -183,34 +186,45 @@ export default function App({navigation}) {
         await axios
           .post(baseURL + '/login', data)
           .then(function (response) {
-            console.log('Login! Token : ', response.data.token);
+            console.log('data를 이렇게 받아옴', response.data);
             userToken = response.data.token;
+            hasRegistered = JSON.stringify(response.data.registerShop);
             AsyncStorage.setItem('userToken', userToken);
-            dispatch({type: 'SIGN_IN', token: 'userToken'});
+            AsyncStorage.setItem('hasRegistered', hasRegistered);
+            dispatch({type: 'SIGN_IN', token: userToken, regi: hasRegistered});
 
             // firebase 토큰 받아오기
-            console.log("토큰 받아오는 중")
-            const firebaseToken = messaging().getToken()
-            .then(async firebaseToken => {
-              console.log("진짜 ㅍㅏ이어베이스토큰이야~~~~", firebaseToken)
-              console.log('유저 토큰은', userToken)
-              console.log('로그인한 상태니까 저장도 해줄게.')
-              saveTokenToDatabase({firebaseToken})
-            })
-            console.log("토큰이다 !!!!!!!", token)
+            console.log('토큰 받아오는 중');
+            const firebaseToken = messaging()
+              .getToken()
+              .then(async firebaseToken => {
+                console.log('진짜 ㅍㅏ이어베이스토큰이야~~~~', firebaseToken);
+                console.log('유저 토큰은', userToken);
+                console.log('로그인한 상태니까 저장도 해줄게.');
+                saveTokenToDatabase({firebaseToken});
+              });
+            console.log('토큰이다 !!!!!!!', token);
           })
           .catch(function (error) {
             console.log(error);
           });
       },
-      signOut: () => dispatch({type: 'SIGN_OUT'}),
+      signOut: async () => {
+        try {
+          await AsyncStorage.removeItem('userToken');
+          await AsyncStorage.removeItem('hasRegistered');
+          dispatch({type: 'SIGN_OUT'});
+        } catch {
+          console.warn('SIGN OUT FAIL!');
+        }
+      },
       signUp: async data => {
         await axios
           .post(baseURL + '/join', data)
           .then(function (response) {
             console.log('Sign Up!', response.data);
             alert('회원가입 성공');
-            dispatch({type: 'SIGN_UP', token: 'null'});
+            dispatch({type: 'SIGN_UP'});
           })
           .catch(function (error) {
             console.log(error);
@@ -244,12 +258,12 @@ export default function App({navigation}) {
                 animationTypeForReplace: state.isSignout ? 'pop' : 'push',
               }}
             />
-          ) : state.storeToken == null ? (
-            // User is signed in && No storeToken
-            <Stack.Screen name="Home" component={Main} />
+          ) : state.hasRegistered == false ? (
+            // User is signed in && No Registration
+            <Stack.Screen name="Home" component={RegiMain} />
           ) : (
-            // storeToken 가지고 있는 상태
-            <Stack.Screen name="Main" component={Main} />
+            // store를 등록한 상태
+            <Stack.Screen name="Main" component={ManageMain} />
           )}
         </Stack.Navigator>
       </NavigationContainer>
