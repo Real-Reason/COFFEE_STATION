5f0e47f38903edb1724182ea030ec284317e06d8
이유진 <eujjin@naver.com>
2021-11-15 23:20:28 +0900

    feat: customer 주변카페 찾기 imgUrl 추가

--- backend/runner/src/main/java/ssafy/runner/domain/repository/ShopImageRepository.java
+++ backend/runner/src/main/java/ssafy/runner/domain/repository/ShopImageRepository.java
@@ -12,4 +12,9 @@ public interface ShopImageRepository extends JpaRepository<ShopImage, Long> {
     @Query("select si.imgUrl from ShopImage si " +
             "where si.id = :shopImageId and si.idx = :index")
     Optional<String> findByIdAndIndex(@Param("shopImageId") Long id, @Param("index") int index);
+
+    @Query("select si.imgUrl from ShopImage si" +
+            " join si.shop s" +
+            " where s.id = :shopId and si.idx = :index")
+    Optional<String> findByShopIdAndIndex(@Param("shopId") Long shopId, @Param("index") int index);
 }
