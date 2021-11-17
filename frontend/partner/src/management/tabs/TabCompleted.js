beebe11e8674e454e7cbbf5a823f94094d5594d6
한건 <ofliberty@naver.com>
2021-11-17 16:52:56 +0900

    #S05P31B203-474 feat: Partner 완료 주문

---
+++ frontend/partner/src/management/tabs/TabCompleted.js
@@ -0,0 +1,68 @@
+import React, {createContext, useEffect, useState} from 'react';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import {View, Text, Button} from 'react-native';
+import styled from 'styled-components/native';
+import axios from 'axios';
+import CompletedOrder from './completedtabs/CompletedOrders';
+
+const BASE_URL = 'http://3.38.99.110:8080/api/partner';
+
+// styled components
+const Container = styled.View`
+  flex-direction: row;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const DeatailContainer = styled(Container)`
+  flex-direction: column;
+`;
+
+//Context 생성
+const TabCompletedContext = createContext();
+
+const Tab = createMaterialTopTabNavigator();
+
+const TabCompleted = ({navigation}) => {
+  const [selectedId, setSelectedId] = useState(null);
+  const [selectedOrder, setSelectedOrder] = useState(null);
+  const [completedOrders, setCompletedOrders] = useState([]);
+  const value = {
+    selectedId,
+    setSelectedId,
+    selectedOrder,
+    setSelectedOrder,
+    completedOrders,
+    setCompletedOrders,
+  };
+
+  // PAID 받아오기
+  const showCompletedOrderList = async () => {
+    try {
+      const response = await axios.get(BASE_URL + '/shop/orders');
+      setCompletedOrders(response.data);
+      console.log('COMPLETED ORDERS RECEIVED');
+    } catch (e) {
+      console.log('COMPLETED 오더 정보 못받아옴', e);
+    }
+  };
+
+  useEffect(() => {
+    showCompletedOrderList();
+  }, []);
+
+  return (
+    <TabCompletedContext.Provider value={value}>
+      <Container>
+        <Tab.Navigator style={{flex: 0.5}}>
+          <Tab.Screen name="완료" component={CompletedOrder} />
+        </Tab.Navigator>
+        <DeatailContainer>
+          <Text>hi</Text>
+        </DeatailContainer>
+      </Container>
+    </TabCompletedContext.Provider>
+  );
+};
+export {TabCompletedContext};
+export default TabCompleted;
