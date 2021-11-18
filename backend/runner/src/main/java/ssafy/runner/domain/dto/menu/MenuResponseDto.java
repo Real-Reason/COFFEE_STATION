c22a7748fd9ba1307d28bf724b6dd9928ce111cb
이정준 <junnnyyy@kakao.com>
2021-11-18 14:10:21 +0900

    #S05P31B203-445 feature:menuResponse수정/be

--- backend/runner/src/main/java/ssafy/runner/domain/dto/menu/MenuResponseDto.java
+++ backend/runner/src/main/java/ssafy/runner/domain/dto/menu/MenuResponseDto.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import ssafy.runner.domain.entity.Menu;
+import ssafy.runner.domain.entity.MenuSize;
+
+import java.util.List;
 
 @Getter @ToString
 @NoArgsConstructor @AllArgsConstructor
@@ -13,15 +16,30 @@ public class MenuResponseDto {
     private Long shopId;
     private String shopName;
     private Long categoryId;
+    private String categoryName;
     private String name;
     private String imgUrl;
     private int price;
     private boolean signature;
     private String menuStatus;
+    MenuSizeListResponseDto menuSizeList;
 
     // 여기서 shop을 조회해서 shopId를 주는 것이 맞는 것인가 고민해보기
     public static MenuResponseDto of(Menu menu) {
-        return new MenuResponseDto(menu.getId(), menu.getShop().getId(), menu.getShop().getName(), menu.getCategory().getId(), menu.getName(), menu.getImgUrl(), menu.getPrice(), menu.isSignature(), menu.getMenuStatus().toString());
+
+        return new MenuResponseDto(
+                menu.getId(),
+                menu.getShop().getId(),
+                menu.getShop().getName(),
+                menu.getCategory().getId(),
+                menu.getCategory().getName(),
+                menu.getName(),
+                menu.getImgUrl(),
+                menu.getPrice(),
+                menu.isSignature(),
+                menu.getMenuStatus().toString(),
+                MenuSizeListResponseDto.of(menu.getMenuSizeList())
+        );
     }
 
 }
